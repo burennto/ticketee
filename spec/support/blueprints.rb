e0ee6180ea4081ee8b8f87eb40295a605f470f2a
Brent Chuang <burennto@gmail.com>
2012-05-06 22:35:22 +1000

    Restrict access to project actions to admins only

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -3,3 +3,9 @@ require 'machinist/active_record'
 Project.blueprint do
   name { 'Ticketee' }
 end
+
+User.blueprint do
+  email                 { "user#{sn}@ticketee.com"}
+  password              { "password"}
+  password_confirmation { "password" }
+end
